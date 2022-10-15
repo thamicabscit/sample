c9871eacbb88dbb14956a3fe2991fafd253508ab
thamicabscit <thamica.thamica@capgemini.com>
2022-10-15 14:18:44 +0530

    ADDED class TEST

---
+++ sampleProj/src/sampleProj/TEST.java
@@ -0,0 +1,15 @@
+package sampleProj;
+
+public class TEST {
+	
+		  int x = 5;
+
+		  public static void main(String[] args) {
+		    TEST myObj1 = new TEST();  // Object 1
+		    TEST myObj2 = new TEST();  // Object 2
+		    System.out.println(myObj1.x);
+		    System.out.println(myObj2.x);
+		  }
+		}
+
+
